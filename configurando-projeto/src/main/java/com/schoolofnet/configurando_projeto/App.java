f5f27ce6082e0c8153afd7f81d4958e8d257a6cb
Thiago Valls <thiago@trinityweb.com.br>
2017-10-30 18:06:03 -0200

    git cache cleared

---
+++ configurando-projeto/src/main/java/com/schoolofnet/configurando_projeto/App.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.configurando_projeto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
