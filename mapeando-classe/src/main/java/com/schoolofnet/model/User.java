e6d0ed7a6d7ea760ca5ab3279314a7befbde43ff
Thiago Valls <thiago@trinityweb.com.br>
2017-10-31 15:05:26 -0200

    mapeando-classe

---
+++ mapeando-classe/src/main/java/com/schoolofnet/model/User.java
@@ -0,0 +1,59 @@
+package com.schoolofnet.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// Columns
+	private Integer id;
+	private String username;
+	private String name;
+	private String lastname;
+	private Date createDate;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public Date getCreateDate() {
+		return createDate;
+	}
+	
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+}
